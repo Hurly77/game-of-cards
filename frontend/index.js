92892e515b6f4be996b102f6ac4bb2ac8f3edec5
Hurly77 <cameron.leverett@yahoo.com>
2020-09-16 13:27:31 -0700

    testing code in devtools

--- frontend/index.js
+++ frontend/index.js
@@ -21,7 +21,7 @@ class Deal extends Deck{
         j = 0,
         tmp = null
     for(i = this.array.length -1; i > 0; i -= 1){
-      
+      j = Math.flo
     }
   }
 }
@@ -35,3 +35,17 @@ class Game {
 class House {
 
 }
+function shuffle(array){
+  let a1 = array,
+      a2 = a1.splice(0, Math.ceil(array.length / 2)),
+      shuffle = []
+  
+  for(i = array.length; i < array.length; i++){
+   shuffle += shuffle.concat.apply([], a1.map((i, ind) => [i, a2[ind]]))
+  }
+  return shuffle
+}
+function shuffle(h1, h2){
+  return [].concat.apply([], h1.map((i, ind) => [i, h2[ind]] ))  
+}
+let array2 = array.splice(0, Math.ceil(array.length / 2));
